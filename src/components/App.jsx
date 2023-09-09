8c69cb72787d515162eaecf68f2ddc8e4ed886cb
Pangpang <vicleeua@gmail.com>
2023-09-09 18:24:46 +0900

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,7 +26,7 @@ export class App extends Component
       }
     }
   
-    componentDidUpdate(prevProps, prevState) {
+    componentDidUpdate(prevState) {
       if (prevState.contacts !== this.state.contacts) {
         localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
       }
